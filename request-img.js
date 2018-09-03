67803f219280013998daefbe91cf699381084d9e
Aaron Parsons <aaron.parsons@live.ca>
2018-09-03 17:55:53 +0000

    completed function to download image

---
+++ request-img.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message', response.statusMessage);
+         console.log('Response Content Type', response.headers['content-type']);
+         console.log('Downloading Image...');
+       })
+       .on('end', function() {
+        console.log('Download Complete.');
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
